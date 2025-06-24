9070d90c6ef5ab48a88f7cdc5d715dd38ec85edc
luislrda28 <luisotaviorsilva@hotmail.com>
2025-06-23 21:17:24 -0300

    Começo

---
+++ src/main/java/com/conectadev/conectadev/ConectaDevApplication.java
@@ -0,0 +1,13 @@
+package com.conectadev.conectadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conectadev/conectadev/ConectaDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conectadev.conectadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
